e5b6f0f12ec02e936e7c60f3f040e239a883be64
PriyankaRumal <priyankarumal182000@gmail.com>
2022-10-30 13:15:45 -0700

    UseCase6 added

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace UC6WagesTillCondi
+{
+    internal class Program
+    {
+        public const int isPartTime = 1;
+        public const int isFullTime = 2;
+        public const int empRatePerHr = 20;
+        public const int noOfWorkingDays = 2;
+        public const int maxHrsInMonth = 10;
+        static void Main(string[] args)
+        {
+            int empHr=0,totalEmpHrs=0,totalWorkingDays=0;
+            while(totalEmpHrs <= maxHrsInMonth && totalWorkingDays < noOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck=random.Next(0,3);
+                switch(empCheck)
+                {
+                    case isPartTime:
+                        empHr = 4;
+                        break;
+                    case isFullTime:
+                        empHr = 8;
+                        break;
+                    default:
+                        empHr = 0;
+                        break;
+
+                }
+                totalEmpHrs=totalEmpHrs+empHr;
+             Console.WriteLine("Days=" + totalWorkingDays      + "Emp Hrs=" + empHr);    
+            }
+            int totalEmpWage = totalEmpHrs * empRatePerHr;
+            Console.WriteLine("Total Emp Wage="+totalEmpWage);
+        }
+    }
+}
