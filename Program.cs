9e689205807cc399a37a9a7b3de14402df244c0b
PriyankaRumal <priyankarumal182000@gmail.com>
2022-10-30 13:39:11 -0700

    usecase7 is added

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace UC7ClassMethod
+{
+    internal class Program
+    {
+        public const int isPartTime = 1;
+        public const int isFullTime = 2;
+        public const int empRatePerHr = 20;
+        public const int noOfWorkingDays = 2;
+        public const int maxHrsInMonth = 10;
+        public static int calculateEmpWage()
+        {
+            int empHr = 0, totalEmpHrs = 0,totalWorkingDays=0;
+            while(totalEmpHrs <= maxHrsInMonth && totalWorkingDays < noOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck=random.Next(0,3);
+                switch(empCheck)
+                {
+                    case isPartTime:
+                        empHr = 4;
+                        break;
+                    case isFullTime:
+                        empHr = 8;
+                        break;
+                    default:
+                        empHr = 0;
+                        break;
+                }
+                totalEmpHrs=totalEmpHrs+empHr;
+                Console.WriteLine("Days=" + totalWorkingDays     + "Emp Hrs=" + empHr); 
+            }
+            int totalEmpWage = totalEmpHrs * empRatePerHr;
+            Console.WriteLine("Total Emp Wage=" + totalEmpWage);
+            return totalEmpWage;
+        }
+        static void Main(string[] args)
+        {
+           calculateEmpWage();
+        }
+    }
+}
