aaf69ace32ec93a0584b55d2616b5d739178ed05
PriyankaRumal <priyankarumal182000@gmail.com>
2022-10-30 12:42:41 -0700

    Use Case2 added

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace UC2DailyWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int wagePerHr = 20;
+            int wage = 0, empHr = 0;
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == 0)
+            {
+                empHr = 8;
+            }
+            else
+            {
+                empHr = 0;
+            }
+
+            wage = wagePerHr * empHr;
+                Console.WriteLine("Total Wage="+wage);
+        }
+    }
+}
