42aa57792ae81a3d5d488e869ba994ec63a129fc
PriyankaRumal <priyankarumal182000@gmail.com>
2022-10-30 12:51:57 -0700

    Use Case3 added

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace UC3PartTimeWagw
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int isPartTime = 1;
+            int isFullTime = 2;
+            int empRatePerHr = 20;
+            int empHr = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck=random.Next(0,3);
+            if (empCheck==isPartTime)
+            {
+                empHr = 4;
+            }
+            else if (empCheck==isFullTime)
+            {
+                empHr = 8;
+            }
+            else
+            {
+                empHr = 0;
+            }
+            empWage = empHr * empRatePerHr;
+            Console.WriteLine("EmpWage="+empWage);
+        }
+    }
+}
